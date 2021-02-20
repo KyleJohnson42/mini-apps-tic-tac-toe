07b6b708f0a1f06c3e49231efb55738dd688aa42
Kyle Johnson <zion4to6@gmail.com>
2021-02-19 18:59:51 -0800

    Wrote win checking and tie checking logic, array clearing bug still exists

--- app.js
+++ app.js
@@ -2,8 +2,59 @@ let board = [[], [], []];
 let currentTurn = 'X';
 let gameWon = false;
 
-let checkWin = board => {
-
+let checkWin = (board, id, player) => {
+  let win = [];
+  // Check rows
+  for (var i = 0; i < 3; i++) {
+    if (board[id[0]][i] !== player) {
+      win = [];
+      break;
+    }
+    win.push(id);
+  }
+  if (win.length === 3) {
+    win.push(player);
+    return win;
+  }
+  // Check columns
+  for (let i = 0; i < 3; i++) {
+    if (board[i][id[1]] !== player) {
+      win = [];
+      break;
+    }
+    win.push(id);
+  }
+  if (win.length === 3) {
+    win.push(player);
+    return win;
+  }
+  // Check major diagonal
+  for (let i = 0, j = 0; i < 3; i++, j++) {
+    if (board[i][j] !== player) {
+      win = [];
+      break;
+    }
+    win.push(id);
+  }
+  if (win.length === 3) {
+    win.push(player);
+    return win;
+  }
+  // Check minor diagonal
+  for (let i = 2, j = 0; i >= 0; i--, j++) {
+    if (board[i][j] !== player) {
+      win = []
+      break;
+    }
+    win.push(id);
+  }
+  if (win.length === 3) {
+    win.push(player);
+    return win;
+  }
+  if (board[0].length + board[1].length + board[2].length === 9){
+    return 'tie';
+  }
 };
 
 let renderBoard = board => {
@@ -15,8 +66,12 @@ let renderBoard = board => {
   }
 }
 
-let renderWin = board => {
+let renderWin = win => {
+  console.log(win[win.length - 1] + ' WINS!')
+}
 
+let renderTie = board => {
+  console.log('TIE!')
 }
 
 let handleClick = event => {
@@ -26,13 +81,29 @@ let handleClick = event => {
       board[id[0]][id[1]] = 'X';
       currentTurn = 'O';
       renderBoard(board);
-      checkWin(board);
+      let won = checkWin(board, id, 'X');
+      if (won) {
+        gameWon = true;
+        if (won === 'tie') {
+          renderTie(board);
+        } else {
+          renderWin(won);
+        }
+      }
     } else if (currentTurn === 'O') {
       let id = event.target.id;
       board[id[0]][id[1]] = 'O';
       currentTurn = 'X';
       renderBoard(board);
-      checkWin(board, id);
+      let won = checkWin(board, id, 'O');
+      if (won) {
+        gameWon = true;
+        if (won === 'tie') {
+          renderTie(board);
+        } else {
+          renderWin(won);
+        }
+      }
     }
   }
 };
@@ -44,6 +115,8 @@ let handleReset = event => {
     }
   }
   renderBoard(board);
+  currentTurn = 'X';
+  gameWon = false;
 }
 
 document.getElementById('board').addEventListener('click', handleClick);
